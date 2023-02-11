445e9b24155fe040e28f7efc7f7e091555ce7932
alxsandoval <alxsandoval@gmail.com>
2023-02-11 00:38:39 -0800

    Fix en el texto del email corregido

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -26,7 +26,7 @@ const emailRegistro = async ( datos ) => {
         subject: 'Comprueba tu cuenta en APV',
         text: 'Comprueba tu cuenta en APV',
         html: `<p>Hola: <b>${nombre}</b>, comprueba tu cuenta en APV.</p>
-            <p>Tu vuenta ya esta lista, solo debes confirmarla en el siguiente enlace:
+            <p>Tu cuenta ya esta lista, solo debes confirmarla en el siguiente enlace:
             <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Confirmar Cuenta</a> </p>
             
             <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje.</p>`
